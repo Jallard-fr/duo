175773144933d8699286fa2462c6388372111a04
Jallard-fr <jbaptiste.allard@gmail.com>
2026-07-27 14:52:19 +0200

    Update __init__.py

--- custom_components/duo/__init__.py
+++ custom_components/duo/__init__.py
@@ -36,7 +36,7 @@ PLATFORMS = ["sensor", "select"]
 # aucune ressource Lovelace à ajouter manuellement.
 URL_BASE = "/duo_frontend"
 CARD_FILE = "duo-card.js"
-CARD_VERSION = "0.1.0"  # à incrémenter à chaque modification du JS
+CARD_VERSION = "0.2.0"  # à incrémenter à chaque modification du JS
 FRONTEND_KEY = f"{DOMAIN}_frontend_registered"
 
 SET_PREFERENCE_SCHEMA = vol.Schema(
